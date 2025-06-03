94b8588c65eeccf130ac8497f979beb8b0788828
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-06-03 01:11:12 -0300

    registrar token

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       post '/usuarios', to: 'usuarios#create'
+      get '/usuarios/:id/confirmar', to: 'usuarios#confirmar', as: :confirmar_usuario
+
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
     end
