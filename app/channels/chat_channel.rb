c429a60c62f98a7c91b8ad4f322a531a190a00a1
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2025-06-03 02:25:08 -0300

    speak mode

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -1,18 +1,21 @@
 class ChatChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "chat_channel"
-  end
+    def subscribed
+        stream_from "chat_channel"
+    end
 
-  def receive(data)
-    usuario = Usuario.find_by(id: connection.current_user&.id)
-    return unless usuario
+    def speak(data)
+        usuario = Usuario.find_by(id: connection.current_user&.id)
+        return unless usuario
 
-    mensagem = usuario.mensagens.create(texto: data['texto'])
-    ActionCable.server.broadcast("chat_channel", {
-      id: mensagem.id,
-      texto: mensagem.texto,
-      usuario: { id: usuario.id, username: usuario.username },
-      created_at: mensagem.created_at.strftime('%H:%M:%S')
-    })
-  end
+        mensagem = usuario.mensagens.create(texto: data['texto'])
+        ActionCable.server.broadcast("chat_channel", {
+            id: mensagem.id,
+            texto: mensagem.texto,
+            usuario: {
+                id: usuario.id,
+                username: usuario.username
+            },
+            created_at: mensagem.created_at.strftime('%H:%M:%S')
+        })
+    end
 end
